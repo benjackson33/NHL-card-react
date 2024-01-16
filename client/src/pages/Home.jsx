bd0928dca9dbd702d8d456c0bfac054be81fbd24
Ben Jackson <benjacksonsmac@gmail.com>
2024-01-16 21:49:33 +1300

    home page additions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,20 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl font-graduate">NHL</h1>
+      <div className="text-center">
+        <img
+          className="w-96"
+          src={`https://assets.nhle.com/logos/nhl/svg/NHL_light.svg`}
+          alt=""
+        />
+      </div>
+      <div>
+        <h1 className="text-5xl font-graduate">HOCKEY</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 };
+
 export default Home;
