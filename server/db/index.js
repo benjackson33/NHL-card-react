4576e827c382eb4412cadfc1e9dec2652077bde4
Ben Jackson <benjacksonsmac@gmail.com>
2024-01-29 18:00:41 +1300

    added database for user details

---
+++ server/db/index.js
@@ -0,0 +1,7 @@
+const pg = require("pg"); //getting postgres
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = db;
---
+++ server/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE nhlcards
+
+create TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
